a2fe704f2f54ea07600c4c7c937424e2a985a07e
Ivo Kosa <ivo.e.kosa@gmail.com>
2024-04-14 15:19:59 +0100

    Added TripoSR

---
+++ src/run.py
@@ -0,0 +1,124 @@
+import argparse
+import logging
+import os
+import time
+
+import numpy as np
+import rembg
+import torch
+from PIL import Image
+
+from tsr.system import TSR
+from tsr.utils import remove_background, resize_foreground, save_video
+
+
+class Timer:
+    def __init__(self):
+        self.items = {}
+        self.time_scale = 1000.0  # ms
+        self.time_unit = "ms"
+
+    def start(self, name: str) -> None:
+        if torch.cuda.is_available():
+            torch.cuda.synchronize()
+        self.items[name] = time.time()
+        logging.info(f"{name} ...")
+
+    def end(self, name: str) -> float:
+        if name not in self.items:
+            return
+        if torch.cuda.is_available():
+            torch.cuda.synchronize()
+        start_time = self.items.pop(name)
+        delta = time.time() - start_time
+        t = delta * self.time_scale
+        logging.info(f"{name} finished in {t:.2f}{self.time_unit}.")
+
+
+class LRM_Reconstruction:
+
+    def __init__(self, remove_bg=True, render=False):
+        self.remove_bg = remove_bg
+        self.render = render
+
+    def runner(self, img_lst):
+        
+        timer = Timer()
+
+        logging.basicConfig(
+            format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO
+        )
+
+        output_dir = "output/"
+        os.makedirs(output_dir, exist_ok=True)
+
+        device = "cpu"
+        # default="cuda:0"
+
+        timer.start("Initializing model")
+        model = TSR.from_pretrained(
+            "stabilityai/TripoSR",
+            config_name="config.yaml",
+            weight_name="model.ckpt",
+        )
+        model.renderer.set_chunk_size(8192)
+        model.to(device)
+        timer.end("Initializing model")
+
+        timer.start("Processing images")
+
+        images = []
+
+        if not self.remove_bg:
+            rembg_session = None
+        else:
+            rembg_session = rembg.new_session()
+
+        for i, image_path in enumerate(img_lst):
+            if self.remove_bg:
+                image = np.array(Image.open(image_path).convert("RGB"))
+            else:
+                image = remove_background(Image.open(image_path), rembg_session)
+                image = resize_foreground(image, 0.85)
+                image = np.array(image).astype(np.float32) / 255.0
+                image = image[:, :, :3] * image[:, :, 3:4] + (1 - image[:, :, 3:4]) * 0.5
+                image = Image.fromarray((image * 255.0).astype(np.uint8))
+                if not os.path.exists(os.path.join(output_dir, str(i))):
+                    os.makedirs(os.path.join(output_dir, str(i)))
+                image.save(os.path.join(output_dir, str(i), f"input.png"))
+            images.append(image)
+        timer.end("Processing images")
+
+        for i, image in enumerate(images):
+            logging.info(f"Running image {i + 1}/{len(images)} ...")
+
+            timer.start("Running model")
+            with torch.no_grad():
+                scene_codes = model([image], device=device)
+            timer.end("Running model")
+
+            if self.render:
+                timer.start("Rendering")
+                render_images = model.render(scene_codes, n_views=30, return_type="pil")
+                for ri, render_image in enumerate(render_images[0]):
+                    render_image.save(os.path.join(output_dir, str(i), f"render_{ri:03d}.png"))
+                save_video(
+                    render_images[0], os.path.join(output_dir, str(i), f"render.mp4"), fps=30
+                )
+                timer.end("Rendering")
+
+            timer.start("Exporting mesh")
+            meshes = model.extract_mesh(scene_codes, resolution=256)
+            final_obj = meshes[0]
+            timer.end("Exporting mesh")
+            return final_obj
+
+
+if __name__ == "__main__":
+
+    files = ['examples/mug1.jpg', 'examples/mug2.jpg', 'examples/mug3.jpg']
+    mug = ['examples/mug1.jpg']
+
+    run = LRM_Reconstruction()
+
+    run.runner(mug)
